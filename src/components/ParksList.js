cf70dbfa5c22286bf0a1021bcd3accdbba77a210
Alan Chelko <achelko@gmail.com>
2022-09-06 18:39:47 +0200

    Minor edits

--- src/components/ParksList.js
+++ src/components/ParksList.js
@@ -8,19 +8,42 @@ class ParksList extends React.Component {
 
 
     return (
-            
+
+      // {this.state.books.length ? (
+      //   <p>Book Carousel coming soon</p>
+      // ) : (
+      //   <h3>No Books Found :(</h3>
+      // )}
+               
       this.props.parksArray.length > 0 && 
         
-        <div>
-          <h2>Parks from your selection</h2>
-          <ListGroup as="ul" numbered style={{ width: "30rem" }}> 
-              {
-                this.props.parksArray.map((park, idx) =>
-                  <ListGroup.Item as="li" key={idx}>{park.parks}</ListGroup.Item>
-                )
-              }
-          </ListGroup> 
-        </div>
+      <div>
+        <h2>Parks from your selection</h2>
+        <ListGroup as="ul" numbered style={{ width: "30rem" }}> 
+            {
+              this.props.parksArray.map((park, idx) =>
+                <ListGroup.Item as="li" key={idx} >{park.parks}</ListGroup.Item>
+              )
+            }
+        </ListGroup> 
+      </div>
+
+      // {this.props.parksArray.length  ? (
+        
+      //   <div>
+      //     <h2>Parks from your selection</h2>
+      //     <ListGroup as="ul" numbered style={{ width: "30rem" }}> 
+      //         {
+      //           this.props.parksArray.map((park, idx) =>
+      //             <ListGroup.Item as="li" key={idx}>{park.parks}</ListGroup.Item>
+      //           )
+      //         }
+      //     </ListGroup> 
+      //   </div>
+      // ) : (
+      // <h3>No match found - please try another selection</h3>
+      // )}
+
 
      );
     }
