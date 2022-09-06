bcf5fd96cff92d42e10993d605704232ffbbcd00
Alan Chelko <achelko@gmail.com>
2022-09-06 14:17:22 +0200

    Commit after fixing select

--- src/Parks.js
+++ src/Parks.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import axios from 'axios';
-import { Form } from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
 import ParksList from './components/ParksList.js';
 import fifty from './data/states.json';
-import activities from './data/states.json';
-
+import activities from './data/activities.json';
+import './parks.css';
 
 
 class Parks extends React.Component {
@@ -12,30 +12,39 @@ class Parks extends React.Component {
     super(props);
     this.state = {
       parkState: '',
+      parkActivity: '',
       // showError: false,
       parks: []
     }
   }
 
-  // handleChange = (e) => {
-  //   e.preventDefault();
-  //   this.setState({
-  //     parkState: e.target.value
-  //   })
-  // };
+  handleState = (e) => {
+    e.preventDefault();
+    this.setState({
+      parkState: e.target.value
+    })
+  };
+  
+
+  handleActivity = (e) => {
+    e.preventDefault();
+    this.setState({
+      parkActivity: e.target.value
+    })
+  };
     
   getParkData = async (e) => {
     e.preventDefault();
-    let selectState = e.target.value;
+    // let selectState = e.target.value;
     // this.setState({parkState: selectState});
          
     try {
-          let parksUrl = `${process.env.REACT_APP_SERVER}/parks?state=${selectState}`
+          let parksUrl = `${process.env.REACT_APP_SERVER}/parks?state=${this.state.parkState}&activities=${this.state.parkActivity}`
           let parksResponse = await axios.get(parksUrl);
           
           this.setState({
-            parks: parksResponse.data,
-            parkState: selectState
+            parks: parksResponse.data
+            // parkState: selectState
           });
         
     } catch (err) {
@@ -49,11 +58,13 @@ class Parks extends React.Component {
     
     console.log(this.state);
     // let objArray = [{value: 'al', state: 'Alabama'}, {value: 'az', state: 'Arizona'}, {value: 'ca', state: 'California'}];
-    let mapIt =  fifty.map((item, idx) =>
+    let mapFifty =  fifty.map((item, idx) =>
     <option value={item.value} key={idx}>{item.state}</option>
-  );
+    );
 
-
+    let mapActivities =  activities.map((item, idx) =>
+    <option value={item.value} key={idx}>{item.activity}</option>
+    );
 
     return (
       <>
@@ -61,40 +72,30 @@ class Parks extends React.Component {
 
         <h3>Choose a park to learn more!</h3>
 
-        <Form>
+        {/* <Form>
          <Form.Control as='select' onChange={this.getParkData}>
           <option>Choose a state to learn more</option>
-          {mapIt}
-          {/* <option value='az'> Arizona </option>
-          <option value='ar'> Arkansas </option>
-          <option value='ca'> California </option>
-          <option value='co'> Colorado </option>
-          <option value='ct'> Connecticut </option>
-          <option value='de'> Delaware </option>
-          <option value='fl'> Florida </option>
-          <option value='ga'> Georgia </option> */}
+          {mapFifty}
          </Form.Control>
-      </Form>
+        </Form> */}
 
-      <Form>
-         <Form.Control as='select' onChange={this.getParkData}>
+        <Form onSubmit={this.getParkData}>
+         <Form.Control as='select' onChange={this.handleState}>
           <option>Choose a state to learn more</option>
-          {mapIt}
-          {/* <option value='az'> Arizona </option>
-          <option value='ar'> Arkansas </option>
-          <option value='ca'> California </option>
-          <option value='co'> Colorado </option>
-          <option value='ct'> Connecticut </option>
-          <option value='de'> Delaware </option>
-          <option value='fl'> Florida </option>
-          <option value='ga'> Georgia </option> */}
+          {mapFifty}
          </Form.Control>
-      </Form>
-     
-            
+         <Form.Control as='select' onChange={this.handleActivity}>
+          <option>Choose an activity to learn more</option>
+          {mapActivities}
+         </Form.Control>
+         <Button id="homeButton" variant="primary" type="submit">Submit!</Button>
+        </Form>
+
+        
+
+                      
       <div>
         <ParksList parksArray={this.state.parks} />
-        {/* <ActivitiesList parksArray={this.state.parks} /> */}
       </div>
 
 
